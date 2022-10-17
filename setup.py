9e1cc5d26f545e4041d85e14be7b9ca5a4631f7a
thesage <thesage@thesage.WORKGROUP>
2022-10-17 18:27:00 +0300

    fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="jsonmodel",
-    version="0.0.7",
+    version="0.0.8",
     description="Setting models to cast to it from json",
     packages=['jsonmodel']
 )
