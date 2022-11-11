053b90a5e7aeb6e773bff5c7db09764acaecef83
thesage <thesage@thesage.WORKGROUP>
2022-11-11 13:52:22 +0300

    dumper

--- jsonmodel/__init__.py
+++ jsonmodel/__init__.py
@@ -1,3 +1,3 @@
-from .jsonmodel import JsonModel, parse_dict, parse_model, parse_list
+from .jsonmodel import JsonModel, parse_dict, parse_model, parse_list, dump_model
 
-__all__ = ["JsonModel", "parse_dict", "parse_model", "parse_list"]
+__all__ = ["JsonModel", "parse_dict", "parse_model", "parse_list", "dump_model"]
